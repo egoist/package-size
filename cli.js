cb924061e4e914da1400b9b94052da0ad673aa63
EGOIST <0x142857@gmail.com>
2016-12-23 23:39:50 +0800

    add update-notifier

--- cli.js
+++ cli.js
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
 'use strict'
 const cac = require('cac')
+const update = require('update-notifier')
 const main = require('./')
 const pkg = require('./package.json')
 
@@ -11,3 +12,5 @@ cli.command('*', pkg.description, main)
 cli.option('--es6', 'Compile the input package down to ES5')
 
 cli.parse()
+
+update({pkg: cli.pkg}).notify()
