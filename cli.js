342fce6ee24d4836c29129006219b09e5d16626d
EGOIST <0x142857@gmail.com>
2016-12-24 16:56:00 +0800

    update cli usage

--- cli.js
+++ cli.js
@@ -2,6 +2,7 @@
 'use strict'
 const cac = require('cac')
 const update = require('update-notifier')
+const chalk = require('chalk')
 const pkg = require('./package.json')
 
 const cli = cac()
@@ -21,6 +22,12 @@ cli.command('*', pkg.description, (input, flags) => {
 
 cli.option('es6', 'Compile the input package down to ES5')
 cli.option('cwd', 'Bundle package in current working directory')
+cli.option('externals', 'Exclude packages from bundled file')
+
+cli.example(`${chalk.yellow('package-size')} react,react-dom`)
+cli.example(`${chalk.yellow('package-size')} styled-jsx/style --externals react`)
+cli.example(`${chalk.yellow('package-size')} ./dist/my-bundle.js --cwd`)
+cli.example(`${chalk.yellow('package-size')} vue@1 angular@1 react@0.14`)
 
 cli.parse()
 
