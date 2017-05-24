a94f1d13a5e967b6660e67351d8278d31973a595
EGOIST <0x142857@gmail.com>
2017-05-24 17:55:49 +0800

    tweak test

--- test.js
+++ test.js
@@ -2,15 +2,11 @@ import test from 'ava'
 import getSizes from './lib'
 
 test('main', async t => {
-  try {
-    const data = await getSizes('vue@2.0.0')
-    t.deepEqual(data, {
-      name: 'vue@2.0.0',
-      size: 188881,
-      minified: 74884,
-      gzipped: 27715
-    })
-  } catch (err) {
-    t.fail(err.message)
-  }
+  const data = await getSizes('vue@2.0.0')
+  t.deepEqual(data, {
+    name: 'vue@2.0.0',
+    size: 188881,
+    minified: 74884,
+    gzipped: 27715
+  })
 })
