474142cfe09b997b6fb73975e49b5d4b312e330b
EGOIST <0x142857@gmail.com>
2018-11-27 13:46:26 +0800

    fix lint

--- cli.js
+++ cli.js
@@ -15,119 +15,117 @@ const pkg = require('./package.json')
 
 const cli = cac()
 
-cli.command('clear-cache', 'Clear the package size cache.')
-.action(() => {
+cli.command('clear-cache', 'Clear the package size cache.').action(() => {
   cache.clear()
   console.log('Done!')
 })
 
-cli.command('[...packages]', pkg.description)
-.action((input, flags) => {
-  if (input.length === 0) return cli.outputHelp()
+cli
+  .command('[...packages]', pkg.description)
+  .action((input, flags) => {
+    if (input.length === 0) return cli.outputHelp()
 
-  if (flags.debug) {
-    process.env.DEBUG = 'package-size'
-  }
-
-  if (flags.analyze) {
-    return require('./lib')(input.join(','), flags)
-  }
-
-  const spinner = ora({ spinner: 'simpleDotsScrolling' })
-
-  const stats = input.map(name => ({
-    name,
-    size: -1,
-    minified: -1,
-    gzipped: -1
-  }))
-
-  let finalTable
-
-  const render = () => {
-    let result = [].concat(stats)
-
-    if (flags.sort) {
-      result = result.sort((a, b) => {
-        return a.gzipped > b.gzipped
-      })
+    if (flags.debug) {
+      process.env.DEBUG = 'package-size'
     }
 
-    const frame = spinner.frame()
+    if (flags.analyze) {
+      return require('./lib')(input.join(','), flags)
+    }
 
-    result = result.map(item => {
-      const prettify = v => (v > 0 ? bytes(v, { unitSeparator: ' ' }) : frame)
+    const spinner = ora({ spinner: 'simpleDotsScrolling' })
 
-      return [
-        `  ${chalk.yellow(item.versionedName || item.name)}`,
-        prettify(item.size),
-        prettify(item.minified),
-        prettify(item.gzipped)
-      ]
-    })
+    const stats = input.map(name => ({
+      name,
+      size: -1,
+      minified: -1,
+      gzipped: -1
+    }))
 
-    result.unshift(
-      ['  package', 'size', 'minified', 'gzipped'].map(v => chalk.bold(v))
+    let finalTable
+
+    const render = () => {
+      let result = [].concat(stats)
+
+      if (flags.sort) {
+        result = result.sort((a, b) => {
+          return a.gzipped > b.gzipped
+        })
+      }
+
+      const frame = spinner.frame()
+
+      result = result.map(item => {
+        const prettify = v => (v > 0 ? bytes(v, { unitSeparator: ' ' }) : frame)
+
+        return [
+          `  ${chalk.yellow(item.versionedName || item.name)}`,
+          prettify(item.size),
+          prettify(item.minified),
+          prettify(item.gzipped)
+        ]
+      })
+
+      result.unshift(
+        ['  package', 'size', 'minified', 'gzipped'].map(v => chalk.bold(v))
+      )
+
+      const table = `\n${createTable(result, { stringLength: getWidth })}\n`
+
+      if (!flags.debug) {
+        logUpdate(table)
+      }
+
+      finalTable = table
+    }
+
+    const build = require('./lib')
+
+    this.timer = setInterval(render, 100)
+
+    Promise.all(
+      input.map((name, index) => {
+        return build(name, flags).then(stat => {
+          stats[index] = stat
+          render()
+        })
+      })
     )
-
-    const table = `\n${createTable(result, { stringLength: getWidth })}\n`
-
-    if (!flags.debug) {
-      logUpdate(table)
-    }
-
-    finalTable = table
-  }
-
-  const build = require('./lib')
-
-  this.timer = setInterval(render, 100)
-
-  Promise.all(
-    input.map((name, index) => {
-      return build(name, flags).then(stat => {
-        stats[index] = stat
-        render()
+      .then(() => clearInterval(this.timer))
+      .then(() => {
+        if (flags.debug) {
+          console.log(finalTable)
+        }
+        if (flags.output) {
+          const outputFile = typeof flags.output === 'string'
+            ? flags.output
+            : 'package-size-output.json'
+          return fs
+            .writeFile(outputFile, JSON.stringify(stats, null, 2), 'utf8')
+            .then(() => {
+              console.log(
+                `> Results have been saved to ${outputFile} in JSON format.`
+              )
+            })
+        }
       })
-    })
-  )
-    .then(() => clearInterval(this.timer))
-    .then(() => {
-      if (flags.debug) {
-        console.log(finalTable)
-      }
-      if (flags.output) {
-        const outputFile = typeof flags.output === 'string'
-          ? flags.output
-          : 'package-size-output.json'
-        return fs
-          .writeFile(outputFile, JSON.stringify(stats, null, 2), 'utf8')
-          .then(() => {
-            console.log(
-              `> Results have been saved to ${outputFile} in JSON format.`
-            )
-          })
-      }
-    })
-    .catch(err => {
-      clearInterval(this.timer)
-      handlerError(err)
-    })
-})
-.option('--debug', 'Show debug output')
-.option('--cwd', 'Bundle package in current working directory')
-.option('--externals <externals>', 'Exclude packages from bundled file')
-.option('--sort', 'Sort packages from small to big bundle size')
-.option('--no-cache', 'Disable module size caching')
-.option('--output', 'Save results to file system in JSON format')
-.option('--analyze', 'Analyze bundled files')
-.example(`  package-size react,react-dom`)
-.example(
-  `  package-size styled-jsx/style --externals react`
-)
-.example(`  package-size ./dist/my-bundle.js`)
-.example(`  package-size local-package --cwd`)
-.example(`  package-size vue@1 angular@1 react@0.14`)
+      .catch(err => {
+        clearInterval(this.timer)
+        handlerError(err)
+      })
+  })
+  .option('--debug', 'Show debug output')
+  .option('--cwd', 'Bundle package in current working directory')
+  .option('--externals <externals>', 'Exclude packages from bundled file')
+  .option('--sort', 'Sort packages from small to big bundle size')
+  .option('--no-cache', 'Disable module size caching')
+  .option('--output', 'Save results to file system in JSON format')
+  .option('--analyze', 'Analyze bundled files')
+  .example(`  package-size react,react-dom`)
+  .example(`  package-size styled-jsx/style --externals react`)
+  .example(`  package-size ./dist/my-bundle.js`)
+  .example(`  package-size local-package --cwd`)
+  .example(`  package-size vue@1 angular@1 react@0.14`)
 
 cli.version(pkg.version)
 cli.help()
