d5677e3465c40645504bbc0bdca049e13f3c9558
EGOIST <0x142857@gmail.com>
2019-03-22 16:54:01 +0800

    test: fuzzy size check

--- __test__/index.test.js
+++ __test__/index.test.js
@@ -3,6 +3,7 @@ const getSizes = require('../lib')
 jest.setTimeout(10000)
 
 test('main', async () => {
-  const data = await getSizes('vue@2.0.0')
+  const { size, ...data } = await getSizes('vue@2.0.0')
   expect(data).toMatchSnapshot()
+  expect(size > 189000 && size < 190000).toBe(true)
 })
